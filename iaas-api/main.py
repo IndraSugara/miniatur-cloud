271ca0c9507a651de960e59eb061a07fc745b23c
Maqlogemink <sughara78@gmail.com>
2026-05-23 22:27:43 +0700

    test

--- iaas-api/main.py
+++ iaas-api/main.py
@@ -35,7 +35,7 @@ def get_db():
     finally:
         db.close()
 
-# ── Authentication ──────────────────────────────────────────────────────
+# ── Auth ──────────────────────────────────────────────────────
 pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2  = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
